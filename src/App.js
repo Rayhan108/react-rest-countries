c1ee18eb23a4b0c7aaa30773df7091cee66f9b61
Rayhan Shorker <raihanshorker56@gmail.com>
2023-03-22 16:54:08 +0600

    (first react practice)

---
+++ src/App.js
@@ -0,0 +1,45 @@
+
+
+import './App.css';
+import Countries from './components/Countries/Countries';
+
+
+
+
+function App() {
+  
+  return (
+    <div className="App">
+<Countries></Countries>
+
+    </div>
+  );
+}
+// function LoadCountries(){
+//   const [countries,setCountries] = useState([])
+//   useEffect(()=>{
+//     fetch('https://restcountries.com/v3.1/all')
+//     .then(res=>res.json())
+//     .then(data=>setCountries(data))
+//   },[])
+//   return(
+//     <div>
+//       <h1>visiting every country</h1>
+//       <h3>
+//         Available countries : {countries.length}
+//       </h3>
+//       {
+//         countries.map(country=> <Country name={country.name.common} population = {country.population}></Country>)
+//       }
+//     </div>
+//   )
+// }
+// function Country(props){
+//   return(
+//     <div>
+//       <h2>Name:{props.name}</h2>
+//       <p>population:{props.population}</p>
+//     </div>
+//   )
+// }
+export default App;
---
+++ src/components/Countries/Country/Country.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Country.css'
+
+const Country = (props) => {
+    // console.log(props.country)
+    const {area,region,population,name,flags} = props.country;
+    return (
+        <div className='country'>
+            <img src={flags.png} alt="" />
+            <h2>Country Name:{name.common}</h2>
+            <p>Area = {area}</p>
+            <p>Region = {region}</p>
+            <p>population = {population}</p>
+        </div>
+    );
+};
+
+export default Country;
